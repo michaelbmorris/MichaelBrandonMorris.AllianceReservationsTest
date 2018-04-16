408d6b881d87e70fd845fb18799dd24f47841bbf
Michael "Brandon" Morris <brandon@michaelbmorris.com>
2018-04-16 12:53:58 -0400

    Add project files.

---
+++ MichaelBrandonMorris.AllianceReservationsTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MichaelBrandonMorris.AllianceReservationsTest
+{
+    public class Class1
+    {
+    }
+}
